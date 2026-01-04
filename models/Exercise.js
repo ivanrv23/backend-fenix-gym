35dbe71a5da9d091c127d9bbbdb7d1b360f8e650
alcicueva <acuevae9@gmail.com>
2026-01-04 15:12:45 -0500

    consulta de ejercicios ok

--- models/Exercise.js
+++ models/Exercise.js
@@ -5,7 +5,8 @@ class Exercise {
   // Obtener lista de ejercicios
   static async getEjercicios() {
     try {
-      const query = `SELECT * FROM exercises e INNER JOIN muscles m ON e.id_muscle = m.id_muscle
+      const query = `SELECT * FROM exercises e INNER JOIN exercise_muscles em ON e.id_exercise = em.id_exercise
+      INNER JOIN muscles m ON em.id_muscle = m.id_muscle INNER JOIN group_muscles g ON m.id_group = g.id_group
       WHERE e.state_exercise = 1 AND m.state_muscle = 1`;
       const rows = await db.execute(query);
       return rows;
