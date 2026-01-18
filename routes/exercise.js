bc7c043447dfabcb8481568215a10198d561a43a
alcicueva <acuevae9@gmail.com>
2026-01-18 13:20:15 -0500

    correccion nombre archivo

--- routes/exercise.js
+++ routes/exercise.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const ExerciseController = require('../controllers/ExerciseController');
+const exerciseController = require('../controllers/exerciseController');
 const auth = require('../middleware/auth');
 
 const router = express.Router();
 
 // Traer lista de metas
-router.get('/lista', auth, ExerciseController.getEjercicios);
+router.get('/lista', auth, exerciseController.getEjercicios);
 
 module.exports = router;
